b4fffd37c980445808916937e041853b28f94146
luis-pedro-dutra-carrocini <luispedrodutracarrocini58@gmail.com>
2025-11-07 19:50:56 -0300

    fix(cors): liberar acesso ao domínio da vercel

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const PORT = process.env.PORT || 5000
+
+const corsOptions = {
+  origin: "https://projeto-simples-front-eight.vercel.app/",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.json({ message: "API funcionando com CI/CD no Render..." })
